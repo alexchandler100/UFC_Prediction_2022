47fcb3430540e2a59b9e123dcc1c2a2bb90e5710
Alex <70060041+alexchandler100@users.noreply.github.com>
2022-03-18 23:57:59 -0700

    added date selection to website and beginning calculate button

--- script.js
+++ script.js
@@ -11,13 +11,20 @@ $(function() {
       const select = document.getElementById('fighters')
       const value = i
       const label = i
-
       select.insertAdjacentHTML('beforeend', `
   <option value="${value}">${label}</option>
 `)
     });
   });
 });
+
+const years = document.getElementById('years')
+for (let i=0;i<30;i++){
+  year = 2022-i
+  years.insertAdjacentHTML('beforeend', `
+<option value="${year}">${year}</option>
+`)
+}
 
 function pickRand(a, b) {
   let x = Math.random();
@@ -28,7 +35,7 @@ function pickRand(a, b) {
   }
 }
 
-function getOption(id,out) {
+function selectFighter(id,out) {
   selectElement = document.querySelector('#'+id);
   output = selectElement.value;
   document.querySelector('.'+out).textContent = output;
@@ -48,6 +55,27 @@ function setFighterImage(i){
   name = name.replace(" ","")
   //sets the image to be the image of the fighter
   document.getElementById("fighter"+i+"pic").src="buildingMLModel/images/"+i+name+".jpg"
+}
+
+function selectDate(monthid,monthout,yearid,yearout) {
+  //sets the image to be the image of the fighter
+  selectMonth = document.querySelector('#'+monthid);
+  output = selectMonth.value;
+  document.querySelector('.'+monthout).textContent = output;
+  selectYear = document.querySelector('#'+yearid);
+  output2 = selectYear.value;
+  document.querySelector('.'+yearout).textContent = output2;
+}
+
+function predictionTuple(fighter1,fighter2,month1,year1,month2,year2){
+  
+  return 'Jon Jones Wins'
+}
+
+function predict(fighter1,fighter2,month1,year1,month2,year2){
+  tup = predictionTuple(fighter1,fighter2,month1,year1,month2,year2)
+  console.log(tup)
+  return tup
 }
 
 
